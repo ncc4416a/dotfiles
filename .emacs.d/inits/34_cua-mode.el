a5a6010f9d575ba8d561564779bf2a5eb2817977
root <root@minerva.lambda.jp>
2014-05-29 14:32:01 +0900

    For Emacs

---
+++ .emacs.d/inits/34_cua-mode.el
@@ -0,0 +1,19 @@
+;; -*- mode: emacs-lisp; coding: utf-8-unix; indent-tabs-mode: nil -*-
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; .emacs.d/inits/34_cua-mode.el
+;;
+;;                       Last Modified: 2014-05-29 14:03:46
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(cua-mode t)
+
+;; デフォルトキーバインドを無効化
+(setq cua-enable-cua-keys nil)
+
+;; キーバインド
+;(define-key global-map (kbd "C-x SPC") 'cua-set-rectangle-mark)
+
+;; .emacs.d/inits/34_cua-mode.el end here
+
+
